7d78ea225ea8ba2387d0731f74e281d73873f87a
Tetrapak0 <112382253+Tetrapak0@users.noreply.github.com>
2023-06-07 18:13:27 +0200

    v2.0 - Added taskkill functionality
    You can now terminate all process instances with CTRL + END, or enable autokill to automatically do that for you.

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,17 @@
 #include <iostream>
 #include <chrono>
 #include <string>
+#include <cctype>
+#include <algorithm>
 
 #pragma warning(disable : 4996)
 
 using namespace std;
+
 bool wasrunning;
+string str;
+bool autoKill;
+bool killed = false;
 
 bool IsProcessRunning(const TCHAR* const executableName) {
     PROCESSENTRY32 entry;
@@ -25,6 +31,20 @@ bool IsProcessRunning(const TCHAR* const executableName) {
         if (!_tcsicmp(entry.szExeFile, executableName)) {
             CloseHandle(snapshot);
             wasrunning = true;
+            string endCommand = "taskkill /f /im \"" + str + "\"";
+            if (!killed)
+            {
+                if (autoKill)
+                {
+                    system(endCommand.c_str());
+                    killed = true;
+                }
+                if (GetKeyState(VK_CONTROL) && GetKeyState(VK_END))
+                {
+                    system(endCommand.c_str());
+                    killed = true;
+                }
+            }
             return true;
         }
     } while (Process32Next(snapshot, &entry));
@@ -42,9 +62,30 @@ int main()
     hInput = GetStdHandle(STD_INPUT_HANDLE);
     GetConsoleMode(hInput, &prev_mode);
     SetConsoleMode(hInput, prev_mode & ENABLE_EXTENDED_FLAGS | (prev_mode & ~ENABLE_QUICK_EDIT_MODE));
+    cout << "WARNING!!!" << endl << "This application has highest level administrator privileges. End system processes at your own risk!" << endl << endl;
+    system("pause");
+    system("cls");
+    cout << "WARNING!!!" << endl << "This application has highest level administrator privileges. End system processes at your own risk!" << endl << endl;
+    cout << "Press CTRL + END to terminate all process instances, if any are running." << endl;
     cout << "Enter the name of an executable, to check for its running instance: ";
-    string str;
     getline(cin, str);
+    string choice;
+    do
+    {
+        cout << "Do you want to automatically terminate all process instances in case any are running? [Y/n]";
+        getline(cin, choice);
+        transform(choice.begin(), choice.end(), choice.begin(), [](unsigned char c) { return tolower(c); });
+        if (choice == "" || choice == "y")
+        {
+            autoKill = true;
+            break;
+        }
+        else if (choice == "n")
+        {
+            autoKill = false;
+            break;
+        }
+    } while (true);
     string newtitle = str + " - CheckProcessRunning";
     SetConsoleTitleA(newtitle.c_str());
     TCHAR* param = new TCHAR[str.size() + 1];
@@ -54,7 +95,7 @@ int main()
     while (true)
     {
         auto now = chrono::system_clock::now();
-        time_t t_c = std::chrono::system_clock::to_time_t(now);
+        time_t t_c = chrono::system_clock::to_time_t(now);
         char* currentTime = ctime(&t_c);
         running = IsProcessRunning(param);
         if (currentTime[strlen(currentTime) - 1] == '\n') currentTime[strlen(currentTime) - 1] = '\0';
---
+++ README.md
@@ -0,0 +1,6 @@
+# CheckProcessRunning
+A simple C++ program to continuously check whether a process is running.
+
+## taskkill
+Press CTRL + END at any time, to terminate all process instances, if any are running.
+You can also enable autokill after entering an executable name. Autokill automatically terminates every instance of the process, if any are running.
